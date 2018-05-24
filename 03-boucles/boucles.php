2368cb475b4b67e18977b96c6121acc2c17924b4
David Serhani <davidserhani@gmail.com>
2018-05-24 14:38:16 +0200

    triangle added

--- 03-boucles/boucles.php
+++ 03-boucles/boucles.php
@@ -1,24 +1,24 @@
 <?php   
-    for ($i = 10; $i > 0; $i--) {
-        echo "$i <br />";
+    for ($y = 10; $y > 0; $y--) {
+        echo "$y <br />";
     }
 
 
-    for ($i = 0; $i <= 100; $i++) {
-        if ($i % 2 == 0) {
-            echo $i;
+    for ($y = 0; $y <= 100; $y++) {
+        if ($y % 2 == 0) {
+            echo $y;
         }
     }
     echo '<br/>';
-    for ($i = 0; $i <= 100; $i++) {
-        if ($i % 3 == 0) {
-            echo 'Fizz <br/>';
-        } elseif ($i % 5 == 0) {
-            echo 'Buzz <br/>';
-        } elseif ($i % 15 == 0) {
+    for ($y = 1; $y <= 100; $y++) {
+        if ($y % 15 == 0) {
             echo 'FizzBuzz <br/>';
+        } elseif ($y % 5 == 0) {
+            echo 'Buzz <br/>';
+        } elseif ($y % 3 == 0) {
+            echo 'Fizz <br/>';
         } else {
-            echo $i;
+            echo $y;
         }
     }
 
@@ -39,6 +39,35 @@
                 echo $pgcd;
             }
         }
+    echo '<br />';
+
+
+    for ($x = 0; $x < 10; $x++) {
+
+        for ($y = 10 ; $y > $x; $y --) {
+
+            echo '🕋';
+        }
+        echo '<br />';
+    }
+
+    $start = 5;
+    $size = 1;
+    for ($y = 0; $y < 6; $y++) {
+        for ($x = 0; $x < 11; $x++) {
+            if ($x == $start) {
+                for ($z = 0; $z < $size; $z++) {
+                    echo '🕋';
+                }
+                $x += $size - 1;
+            } else {
+                echo '📖';
+            }
+        }
+        $start--;
+        $size += 2;
+        echo '<br />';
+    }
 
 
 
