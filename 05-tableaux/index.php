3f33181043a099504fc6f760a692c9ef0e2b103d
David Serhani <davidserhani@gmail.com>
2018-05-25 09:56:49 +0200

    array added

---
+++ 05-tableaux/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $people = [
+      'Jean',
+      'Eric',
+      'Jeanne'
+    ];
+
+//    echo $people; impossible
+    echo '<br />';
+    echo '<pre>';
+    print_r($people);
+    echo '</pre>';
+
+    var_dump($people);
