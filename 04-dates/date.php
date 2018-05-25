7cf2984eefa065fd59dfb899d23bc0725688e142
David Serhani <davidserhani@gmail.com>
2018-05-25 16:59:13 +0200

    array exercices added

--- 04-dates/date.php
+++ 04-dates/date.php
@@ -1,5 +1,5 @@
 <?php
-    $today = date('l d F Y') . ', il est ' . date('H\hi');
-    echo $today .'<br/>';
-    $futur = strtotime('3 june 2018');
+    $t = date('l d F Y') . ', il est ' . date('H\hi');
+    echo $t .'<br/>';
+    $futur = strtotime('+ 10 days');
     echo 'Dans 10 jours nous serons un ' . date('l', $futur);
--- 05-tableaux/index.php
+++ 05-tableaux/index.php
@@ -3,7 +3,8 @@
     $people = [
       'Jean',
       'Eric',
-      'Jeanne'
+      'Jeanne',
+      'John'
     ];
 
 //    echo $people; impossible
@@ -13,3 +14,108 @@
     echo '</pre>';
 
     var_dump($people);
+    echo '<br />';
+    echo $people[1];
+    echo '<br /> ----- FOREACH ----- <br />';
+
+    foreach ($people as $index => $person) {
+        echo $index . ':' . $person . '<br />';
+    }
+    echo '<br /> ----- END OF FOREACH ----- <br />';
+    $people = [
+        'Jean',
+        3 => 'Eric',
+        'Jeanne'
+    ];
+    var_dump($people);
+
+    $people = [
+      [
+        'name' => 'Serhani',
+        'first_name' => 'David',
+        'age' => 33,
+        'phone' => [
+            'cellular' => 06060606,
+            'home_phone' => 032020202
+        ]
+      ],
+        [
+            'name' => 'Mota',
+            'first_name' => 'Matthieu',
+            'age' => 50,
+            'phone' => [
+                'cellular' => '07050203',
+                'home_phone' => '0321200202'
+            ]
+        ]
+    ];
+    var_dump($people);
+    echo '<br />';
+    foreach ($people as $person) {
+        echo "{$person['first_name']} a {$person['age']} et est joignable au : ";
+        foreach ($person['phone'] as $type => $number) {
+            echo "{$type} - {$number} <br />";
+        }
+    }
+
+    echo '<br />';
+
+    $students = [
+        0 => [
+            'nom' => 'Matthieu',
+            'notes' => [10, 8, 16, 16, 15, 2]
+        ],
+        1 => [
+            'nom' => 'Thomas',
+            'notes' => [4, 8, 6, 5, 2]
+        ],
+        2 => [
+            'nom' => 'Jean',
+            'notes' => [10, 8, 15, 9, 20]
+        ]
+    ];
+
+    foreach ($students as $student) {
+        echo "Les notes de {$student['nom']} : ";
+        foreach ($student['notes'] as $note) {
+            echo "{$note}; ";
+        }
+        echo '<br />';
+    }
+
+
+    $average = 0;
+    foreach ($students as $student) {
+        $total = 0;
+        foreach ($student['notes'] as $note) {
+            $total += $note;
+        }
+        $averages = $total / count($student['notes']);
+        if ($averages > 10) {
+            $average++;
+            echo "{$student['nom']} a la moyenne !<br />";
+        } else {
+            echo "{$student['nom']} n'a pas la moyenne ...<br />";
+        }
+    }
+    echo "Eleve(s) avec la moyenne : {$average} <br />";
+
+    $best = array();
+    $max = 0;
+    foreach ($students as $student) {
+        $max = max($student['notes']);
+        array_push($best, $max);
+    }
+    foreach ($students as $student) {
+        foreach ($student['notes'] as $note) {
+
+        }
+    }
+
+
+
+
+
+
+
+
